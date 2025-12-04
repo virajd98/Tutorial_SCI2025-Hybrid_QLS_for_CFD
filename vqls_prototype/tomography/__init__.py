d0c38f96843bbf0dbef73c452ad1d9c3f26275e4
DenisPoisson <atharvmailbox3@gmail.com>
2025-12-04 16:24:20 +0530

    Clean up

--- vqls_prototype/tomography/__init__.py
+++
@@ -1,4 +0,0 @@
-from .qst import FullQST
-from .htree_qst import HTreeQST
-from .simulator_qst import SimulatorQST
-from .shadow_qst import ShadowQST
--- vqls_prototype/tomography/nnqst.py
+++
@@ -1,9 +0,0 @@
-def get_relative_amplitude_sign(circuit, parameters):
-    """_summary_
-
-    Args:
-        circuit (_type_): _description_
-        parameters (_type_): _description_
-        backend (_type_): _description_
-    """
-    raise NotImplementedError("NNQST not implemented")
